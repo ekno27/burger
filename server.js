b69e26ba6881f09741ebd38414af3743ed90e07a
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-25 15:50:22 -0700

    added front end

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 //allowing styles n whatnot
-
+// app.use('/img', express.static(path.join(__dirname, '/public/assets/img')));
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 

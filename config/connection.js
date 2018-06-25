d292a681d40ff2275a99f8ed54d30079ab13f2b0
ekno27 <edgar.canozelaya.6@my.csun.edu>
2018-06-25 16:05:54 -0700

    jaws db setup

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: process.env.port || 3306,
-    user: "root",
-    password: "root.",
-    database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.connection({
+        host: "localhost",
+        port: process.env.port || 3306,
+        user: "root",
+        password: "root.",
+        database: "burgers_db"
+    });
+   
+}
 
 //make a connection
 connection.connect(function(err){
